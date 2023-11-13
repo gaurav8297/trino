c46c8997f7e829766c21dd4e614338c048183377
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2023-11-12 20:20:55 -0500

    Use concurrent hash map for tracking query memory
    This will increase the performance of
    getQueryMemoryReservation(queryId) method since
    that is called from every different
    local exchange thread.

--- core/trino-main/src/main/java/io/trino/memory/MemoryPool.java
+++ core/trino-main/src/main/java/io/trino/memory/MemoryPool.java
@@ -31,6 +31,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
@@ -55,7 +56,7 @@ public class MemoryPool
 
     @GuardedBy("this")
     // TODO: It would be better if we just tracked QueryContexts, but their lifecycle is managed by a weak reference, so we can't do that
-    private final Map<QueryId, Long> queryMemoryReservations = new HashMap<>();
+    private final Map<QueryId, Long> queryMemoryReservations = new ConcurrentHashMap<>();
 
     // This map keeps track of all the tagged allocations, e.g., query-1 -> ['TableScanOperator': 10MB, 'LazyOutputBuffer': 5MB, ...]
     @GuardedBy("this")
@@ -347,7 +348,7 @@ public class MemoryPool
         return reservedRevocableBytes;
     }
 
-    synchronized long getQueryMemoryReservation(QueryId queryId)
+    long getQueryMemoryReservation(QueryId queryId)
     {
         return queryMemoryReservations.getOrDefault(queryId, 0L);
     }
